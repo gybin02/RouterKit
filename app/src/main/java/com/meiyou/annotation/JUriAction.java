cd7d866c1bb63094374c358ab45443b716627619
zhengxiaobin <zhengxiaobin@linggan.com>
2017-07-13 13:46:41 +0800

    1. 新增 测试 @JUri 和@JUriAction

---
+++ app/src/main/java/com/meiyou/annotation/JUriAction.java
@@ -0,0 +1,15 @@
+package com.meiyou.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author zhengxiaobin@xiaoyouzi.com
+ * @since 17/7/13
+ */
+@Target(ElementType.TYPE) // 代表在类级别上才能使用该注解
+@Retention(RetentionPolicy.SOURCE)
+public @interface JUriAction {
+}
