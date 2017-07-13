b8d4799d07f15e7da8a144c2671dd93f18c76d3b
zhengxiaobin <zhengxiaobin@linggan.com>
2017-07-13 16:42:17 +0800

    1. add 初始化Route；

--- app/src/main/java/com/seeker/tony/myapplication/MyApplication.java
+++ app/src/main/java/com/seeker/tony/myapplication/MyApplication.java
@@ -3,6 +3,8 @@ package com.seeker.tony.myapplication;
 import android.app.Application;
 import android.content.Context;
 
+import com.seeker.tony.myapplication.route.Route;
+
 /**
  * @author zhengxiaobin@xiaoyouzi.com
  * @since 17/7/13
@@ -15,6 +17,7 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = this.getApplicationContext();
+        Route.getInstance().init(this);
     }
 
     public static Context getContext() {
