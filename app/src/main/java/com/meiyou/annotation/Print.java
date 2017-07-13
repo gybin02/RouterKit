8c75312a572574b3d589bbcfd35fc2150e20ae3d
zhengxiaobin <zhengxiaobin@linggan.com>
2017-07-13 11:58:06 +0800

    1. 新增测试注解；

---
+++ app/src/main/java/com/meiyou/annotation/Print.java
@@ -0,0 +1,11 @@
+package com.meiyou.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD})  
+@Retention(RetentionPolicy.CLASS)                                  
+public @interface Print {                                     
+}
