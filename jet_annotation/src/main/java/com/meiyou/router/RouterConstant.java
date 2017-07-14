5e29846d01ac7cb5a15286df71b8940e3e18e11d
zhengxiaobin <zhengxiaobin@linggan.com>
2017-07-14 16:23:01 +0800

    1. jet_annotation 初始化；

---
+++ jet_annotation/src/main/java/com/meiyou/router/RouterConstant.java
@@ -0,0 +1,14 @@
+package com.meiyou.router;
+
+/**
+ * 默认变量
+ *
+ * @author zhengxiaobin@xiaoyouzi.com
+ * @since 17/7/14
+ */
+
+public class RouterConstant {ç
+
+    public static final String router_pkg = "com.meiyou.router.data";
+    public static final String router_class = "RouterTable";
+}
