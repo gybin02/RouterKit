7280fe121ecc8884e9bf3cfecc9c2e1b98eb5719
zhengxiaobin <zhengxiaobin@linggan.com>
2017-07-14 16:23:22 +0800

    1. 生成Java文件范例l

--- jet_compiler/src/main/java/com/meiyou/temp/JUriTemp.java
+++ jet_compiler/src/main/java/com/meiyou/temp/JUriTemp.java
@@ -2,16 +2,21 @@ package com.meiyou.temp;
 
 import java.util.HashMap;
 
+import com.meiyou.router.model.RouteBean;
+import com.meiyou.router.model.RouteType;
+
 /**
  * @author zhengxiaobin@xiaoyouzi.com
  * @since 17/7/13
  */
 
 public class JUriTemp {
-    public static final HashMap<String, String> map = new HashMap();
+
+    public static final HashMap<String, RouteBean> map = new HashMap<>();
 
     static {
-        map.put("meiyou:///home", "com.seeker.tony.myapplication.IntentActivity");
+        RouteBean.createBean(map, "/home", "com.seeker.tony.myapplication.IntentActivity", RouteType.UI);
+        RouteBean.createBean(map, "/home/action", "com.seeker.tony.myapplication.action.TestAction", RouteType.METHOD);
     }
     
 
